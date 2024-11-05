2435f927245f32de5a47ad80f9203d98f15e2028
edolivera <contatodoed@hotmail.com>
2024-11-05 09:38:10 -0300

    Página em Construção

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import MainNav from './components/MainNav';
 import './App.css';
 import Canvas from './components/Canvas';
+import UncontrolledExample from './components/Carousel'; // Atualizado para o nome do seu componente
+
 
 function App() {
   return (
@@ -8,6 +10,7 @@ function App() {
       <MainNav />
       <header className="">      
       </header>
+      <UncontrolledExample /> 
       <Canvas />
     </div>
   );
